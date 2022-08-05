e4349004d62154d1f7f9bad54cbb716272020dcf
Devanshi Gupta <devanshigupta2002@gmail.com>
2022-08-05 12:37:12 +0530

    second commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import Image from '../assets/img/house-banner.png'
+import search from '../components/Search'
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return ( 
+  <section className='h-full max-h-[640px] mb-8 xl:mb-24'>
+    <div className='flex flex-col lg:flex-row'>
+      <div>
+        <h1>
+          <span>Search Properties to Rent</span>
+        </h1>
+        <p>
+          Discover a place you'll love to live. We will help you find a home that is perfect for you!
+        </p>
+      </div>
+      {/* image */}
+      <div>
+        <img src={Image} alt='' />
+      </div>
+    </div>
+    <search/>
+  </section>
+  
+  );
 };
 
 export default Banner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import Banner from '../components/Banner'
+
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='min-h-[1800px]'>
+      <Banner/>
+    </div>
+  );
 };
 
 export default Home;
